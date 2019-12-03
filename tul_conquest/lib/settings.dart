8368f4f7b3131f241348484da884ccaeea919a23
Jakub Bogdan <kubab1998@o2.pl>
2019-12-03 20:59:31 +0100

    Nie no idziesz do domu teraz
    Ustawienia i sklep pani Basi

--- tul_conquest/lib/settings.dart
+++ tul_conquest/lib/settings.dart
@@ -2,7 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'homescreen.dart';
 
-bool wibracje = false;
+bool wibracje = true;
+bool dzwiek = false;
+
+//class Settings extends StatefulWidget {
+//  @override
+//   createState() => HomeScreenState();
+//}
 
 class SettingsRoute extends StatelessWidget {
   @override
@@ -12,16 +18,35 @@ class SettingsRoute extends StatelessWidget {
           title: Text("Ustawienia"), backgroundColor: Colors.indigo[800]),
       body: Center(
           child: Container(
-              width: MediaQuery.of(context).size.width * 0.75,
-              child: SwitchListTile(
-                value: wibracje,
-                title: Text("Wibracje",
-                    style:
-                        TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
-                onChanged: (bool value) {
-                    wibracje = value;
-                },
-              ))),
+              width: MediaQuery.of(context).size.width * 0.85,
+              padding: EdgeInsets.fromLTRB(0, 30, 0, 30),
+              child: ListView(children: <Widget>[
+                SwitchListTile(
+                  value: wibracje,
+                  title: Text("Wibracje",
+                      style:
+                          TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
+                  onChanged: (bool value) {
+                   // setState(() {
+                      wibracje = value;
+                    //}
+                  },
+                  secondary: const Icon(Icons.vibration),
+                ),
+                Divider(),
+                SwitchListTile(
+                  value: dzwiek,
+                  title: Text("Dźwięk",
+                  style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold
+                  ),),
+                  onChanged: (bool value) {
+                    dzwiek = value;
+                  },
+                    secondary: const Icon(Icons.headset)
+                ),
+              ]))),
     );
   }
 }
--- tul_conquest/lib/store.dart
+++ tul_conquest/lib/store.dart
@@ -1,22 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
+int cena = 5;
+
 class StoreRoute extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Sklep"),
-          backgroundColor: Colors.indigo[800]
-      ),
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: Text('Go back!'),
-        ),
-      ),
+      appBar: AppBar(title: Text("Sklep"), backgroundColor: Colors.indigo[800]),
+      body: GestureDetector(
+        onTap: () {
+          // KUPOWANIE
+        },
+          child: Align(
+              alignment: Alignment.center,
+              child: Container(
+                  width: MediaQuery.of(context).size.width * 0.85,
+                  padding: EdgeInsets.fromLTRB(0, 30, 0, 30),
+                  child: ListView(children: <Widget>[
+                    Card(
+                        child: ListTile(
+                      leading: Icon(Icons.add_location),
+                      title: Text(
+                        'Dokup znaczniki',
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold),
+                      ),
+                      subtitle: Text('Cena: $cena ECTS',
+                      style: TextStyle(
+                        fontSize: 15
+                      ),),
+                      trailing: Icon(Icons.shopping_cart),
+                    ))
+                  ])))),
     );
   }
 }
