87979b01e53ed1216494c5533095eeb1f1cf8282
Szymon <s.rutkowski235@gmail.com>
2019-12-03 13:52:43 +0100

    zapisywanie i odczytywanie z pliku jeszcze do sprawdzenia

--- tul_conquest/lib/profilescreen.dart
+++ tul_conquest/lib/profilescreen.dart
@@ -1,6 +1,8 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
 
-
+int points=3;
 class ProfileScreen extends StatelessWidget {
 
   @override
@@ -12,7 +14,9 @@ class ProfileScreen extends StatelessWidget {
         ),
         body: Center(
           child: RaisedButton(
+            splashColor: Colors.cyan,
             onPressed: () {
+              writeCounter(points);
               Navigator.pop(context);
             },
             child: Text('Go back!'),
@@ -21,4 +25,33 @@ class ProfileScreen extends StatelessWidget {
   }
 }
 
+
+Future<String> get _localPath async {
+    final directory = await getApplicationDocumentsDirectory();
+
+    return directory.path;
+}
+Future<File> get _localFile async {
+  final path = await _localPath;
+  return File('$path/counter.txt');
+}
+Future<File> writeCounter(int counter) async {
+  final file = await _localFile;
+
+  // Write the file.
+  return file.writeAsString('$counter');
+}
+Future<int> readCounter() async {
+  try {
+    final file = await _localFile;
+
+    // Read the file.
+    String contents = await file.readAsString();
+
+    return int.parse(contents);
+  } catch (e) {
+    // If encountering an error, return 0.
+    return 0;
+  }
+}
 
